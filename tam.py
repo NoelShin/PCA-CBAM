76a46fcbe3d86330d07a294901b7d9dad4b71594
Noel <36836739+NoelShin@users.noreply.github.com>
2019-09-16 22:49:25 +0900

    Update tam.py

--- tam.py
+++ tam.py
@@ -1,17 +1,22 @@
 import torch.nn as nn
+from math import log
 
 
 class TAM(nn.Module):
-    def __init__(self, n_ch, conversion_factor):
+    def __init__(self, n_ch, group_size):
         super(TAM, self).__init__()
         tam = []
-        for i in range(conversion_factor):
-            tam += [nn.Conv2d(n_ch, n_ch // 2, 1, groups=n_ch // 2)]
-            n_ch //= 2
-            if i != conversion_factor - 1:
-                tam += [nn.PReLU(n_ch, init=0.0)]
+        for i in range(int(log(n_ch) / log(group_size))):
+            tam += [nn.Conv2d(n_ch, n_ch // group_size, 1, groups=n_ch // group_size)]
+            n_ch //= group_size
+            if n_ch == 1:
+                break
+            tam += [nn.PReLU(n_ch, init=0.0)]
+
+        tam += [nn.Conv2d(n_ch, 1, 1)] if n_ch != 1 else []
         tam += [nn.Sigmoid()]
         self.tam = nn.Sequential(*tam)
+        print(self)
 
     def forward(self, x):
         return x * self.tam(x)
